d7d83de1ed103124f5d41f7af17109e605a349ce
supersem <xsupersemx@gmail.com>
2020-03-16 17:40:26 +0200

    init commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
+
+getResource('https://swapi.co/api/people/1/')
+  .then((body) => {
+    console.log(body);
+  });
