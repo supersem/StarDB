fa1dec28745e5506e8ae2fc5bf3e16720d0521f6
supersem <xsupersemx@gmail.com>
2020-03-16 22:35:59 +0200

    add components

---
+++ src/components/random-planet/random-planet.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import './random-planet.css';
+
+export default class RandomPlanet extends Component {
+
+  render() {
+    return (
+      <div className="random-planet jumbotron rounded">
+        <img className="planet-image"
+             src={`https://starwars-visualguide.com/assets/img/planets/5.jpg`} />
+        <div>
+          <h4>name</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Population</span>
+              <span>population</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation Period</span>
+              <span>rotationPerio}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diameter</span>
+              <span>diameter</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-const getResource = async (url) => {
-  const res = await fetch(url);
-  const body = await res.json();
-  return body;
-};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-getResource('https://swapi.co/api/people/1/')
-  .then((body) => {
-    console.log(body);
-  });
+ReactDOM.render(<App />,
+  document.getElementById('root'));
