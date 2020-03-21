b62217e99210bdf670c8f62a3a6ab38c9fb27cda
supersem <xsupersemx@gmail.com>
2020-03-21 12:47:53 +0200

    add error handling

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Spinner from '../spinner';
 
+import ErrorIndicator from '../error-indicator';
+
 import SwapiService from '../../services/swapi-service';
 
 import './random-planet.css';
@@ -18,31 +20,47 @@ export default class RandomPlanet extends Component {
   constructor() {
     super();
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 3500);
+    clearInterval(this.interval);
   }
 
   onPlanetLoaded = (planet) => {
     this.setState({
       planet,
-      loading: false
+      loading: false,
+      error: false
     });
   };
 
-  updatePlanet() {
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false
+    })
+  };
+
+  updatePlanet = () => {
+    console.log('update');
     const id = Math.floor(Math.random()*25 + 2);
     this.swapiService
       .getPlanet(id)
-      .then(this.onPlanetLoaded);
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
 
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
 
+    const hasData = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator/> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView  planet={planet}/> : null;
+    const content = hasData ? <PlanetView  planet={planet}/> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
       </div>
@@ -57,7 +75,7 @@ const PlanetView = ({ planet }) => {
   return (
     <React.Fragment>
       <img className="planet-image"
-         src={`https://starwars-visualguide.com/assets/img/planets/5.jpg`} />
+         src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
       <div>
       <h4>{name}</h4>
       <ul className="list-group list-group-flush">
