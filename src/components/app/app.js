004ec2605bf16c606e298fe94d1e1f8cea3a7bf9
supersem <xsupersemx@gmail.com>
2020-03-29 21:43:47 +0300

    change some functions to arrow functions

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,10 +5,15 @@ import RandomPlanet from '../random-planet';
 import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator'
 import PeoplePage from '../people-page';
+import SwapiService from '../../services/swapi-service';
+import ItemList from '../item-list/item-list';
+import PersonDetails from '../person-details/person-details';
 
 import './app.css';
 
 export default class App extends Component {
+
+  swapiService = new SwapiService();
 
   state = {
     showRandomPlanet: true,
@@ -57,8 +62,17 @@ export default class App extends Component {
         </div>
 
         <PeoplePage />
-        <PeoplePage />
-        <PeoplePage />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson}/>
+          </div>
+        </div>
 
       </div>
     );
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -8,18 +8,18 @@ import './item-list.css';
 
 export default class ItemList extends Component {
 
-  swapiService = new SwapiService();
-
   state = {
-    peopleList: null
+    itemList: null
   };
 
   componentDidMount() {
-    this.swapiService
-      .getAllPeople()
-      .then((peopleList) => {
+
+    const { getData } = this.props;
+
+    getData()
+      .then((itemList) => {
         this.setState({
-          peopleList
+          itemList
         });
       });
   };
@@ -38,13 +38,13 @@ export default class ItemList extends Component {
 
   render() {
 
-    const { peopleList} = this.state;
+    const { itemList} = this.state;
 
-    if (!peopleList) {
-      return <Spinner />; 
+    if (!itemList) {
+      return <Spinner />;
     }
 
-    const items = this.renderItems(peopleList);
+    const items = this.renderItems(itemList);
 
     return (
       <ul className="item-list list-group">
